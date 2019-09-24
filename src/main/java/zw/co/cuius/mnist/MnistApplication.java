de061fb4d2400c36874c71cef7f38f132b189269
Terrence <terrence@rubieminnovations.xyz>
2019-09-24 10:56:30 +0200

    Project Set Up

---
+++ src/main/java/zw/co/cuius/mnist/MnistApplication.java
@@ -0,0 +1,13 @@
+package zw.co.cuius.mnist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MnistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MnistApplication.class, args);
+	}
+
+}
